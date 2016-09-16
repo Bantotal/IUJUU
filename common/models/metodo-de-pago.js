9e35b135069bd9f5bd9c77cd52dfd092ad50b9f7
Martin Varela <martinvarela92@icloud.com>
2016-09-15 23:42:30 -0500

    definicion de apis

--- common/models/metodo-de-pago.js
+++ common/models/metodo-de-pago.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Metododepago) {
+	Metododepago.disableRemoteMethod("create", true);
+	Metododepago.disableRemoteMethod("upsert", true);
+	Metododepago.disableRemoteMethod("updateAll", true);
+	Metododepago.disableRemoteMethod("updateAttributes", false);
+	Metododepago.disableRemoteMethod("createChangeStream", true);
+	Metododepago.disableRemoteMethod("replaceOrCreate", true);
+	Metododepago.disableRemoteMethod("upsertWithWhere", true);
+	Metododepago.disableRemoteMethod("replaceById", true);
 
+	Metododepago.disableRemoteMethod("deleteById", true);
+	 
+	Metododepago.disableRemoteMethod("confirm", true);
+	Metododepago.disableRemoteMethod("count", true);
+	Metododepago.disableRemoteMethod("exists", true);
 };
--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Usuario) {
+	Usuario.disableRemoteMethod("create", false);
+	Usuario.disableRemoteMethod("upsert", true);
+	Usuario.disableRemoteMethod("updateAll", true);
+	Usuario.disableRemoteMethod("updateAttributes", false);
+	Usuario.disableRemoteMethod("createChangeStream", true);
+	Usuario.disableRemoteMethod("replaceOrCreate", true);
+	Usuario.disableRemoteMethod("upsertWithWhere", true);
+	Usuario.disableRemoteMethod("replaceById", true);
+
+	Usuario.disableRemoteMethod("find", true);
+	Usuario.disableRemoteMethod("findById", true);
+	Usuario.disableRemoteMethod("findOne", true);
+	 
+	Usuario.disableRemoteMethod("deleteById", true);
+	 
+	Usuario.disableRemoteMethod("confirm", true);
+	Usuario.disableRemoteMethod("count", true);
+	Usuario.disableRemoteMethod("exists", true);
 
 };
