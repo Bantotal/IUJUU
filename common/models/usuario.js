3f15d77fb6bb1adbc5f4301362e8a7eca462375b
Santiago Ravera <raverasantiago@gmail.com>
2016-10-17 14:48:02 -0300

    fix

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -525,9 +525,6 @@ module.exports = function(Usuario) {
     );
 
 		
-
-};
-
 function enviarMail(id, regaloId, email, cb) {
 
       var Pago = app.models.Pago;
@@ -601,3 +598,5 @@ function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+};
